17f237853a417a7cf594c73e8765ec7b5e4381f5
MauricioMoldes <mauriciomoldesquaresma@gmail.com>
2025-11-07 14:35:06 +0100

    variant_hashes optimization

--- variant_hashes.py
+++ variant_hashes.py
@@ -1,176 +1,142 @@
+#!/usr/bin/env python3
 import os
 import sys
 import logging
 import argparse
 import pathlib
-
 import numpy
 
 import data_parser
 
-# set up logger, using inherited config, in case we get called as a module
+# Logger setup — inherits root config if used as a module
 logger = logging.getLogger(__name__)
-
 
 CLUSTER_HASH_LENGTH = 20
 
 
 def generate_cluster_hashes(clusters):
     """
-    Generate a unique identifier, "cluster hash", for every cluster,
-    ie a string with CLUSTER_HASH_LENGTH digits (0/1s)
-    
-    arguments:
-    - clusters: list of cluster IDs
+    Generate a unique binary hash for each cluster.
 
-    returns:
-    - cluster2hash: dict, key=clusterID, value=hash
+    Args:
+        clusters (list): Cluster IDs.
+    Returns:
+        dict: clusterID -> binary hash (string of 0/1s)
     """
     cluster2hash = {}
-
-    idx = 0  # populating cluster hashes from the end
-    for cluster in clusters:
-        hash = numpy.binary_repr(idx, width=CLUSTER_HASH_LENGTH)
-        cluster2hash[cluster] = hash
-        idx += 1
-
-    return(cluster2hash)
+    for idx, cluster in enumerate(clusters):
+        cluster2hash[cluster] = numpy.binary_repr(idx, width=CLUSTER_HASH_LENGTH)
+    return cluster2hash
 
 
 def generate_individual_hashes(individual2cluster, cluster2hash, variant2hash, haploblock2hash, chr_hash):
     """
-    Generate individual hashes, ie 64-character strings of 0/1s, each contains:
-        strand hash: 4 char
-        chromosome hash: 10 chars
-        haploblock hash: len(haploblocks) chars
-        cluster hash: len(clusters) chars
-        variant hash: len(variants of interest) chars
+    Generate a 64-character binary hash per individual.
 
-    arguments:
-    - individual2cluster: dict, key=individual, value=unique clusterID
-    - cluster2hash: dict, key=clusterID, value=hash
-    - variant2hash: dict, key=individual, key=hash
-    - haploblock2hash: dict, key=(start, end), key=hash
-    - chr_hash: 10-digit
-
-    returns:
-    - individual2hash: dict, key=individual, value=hash
+    Hash structure:
+      - Strand hash: 4 bits
+      - Chromosome hash: 10 bits
+      - Haploblock hash: variable length
+      - Cluster hash: 20 bits
+      - Variant hash: variable length
     """
     individual2hash = {}
 
-    for individual in individual2cluster:
-        # strand hash
-        # individual is in format: NA18524_chr6_region_31480875-31598421_hap0
-        # this should be fixed in the future, for now taking the last char from individual as strand (can be 0 or 1)
+    for individual, cluster in individual2cluster.items():
+        # Strand hash
         strand = individual[-1]
-        strand_hash = ""
-        if strand == "0":
-            strand_hash = "0001"
-        else:
-            strand_hash = "0010"
+        strand_hash = "0001" if strand == "0" else "0010"
 
-        # haploblock_hash
-        # individual is in format: NA18524_chr6_region_31480875-31598421_hap0
-        # this should be fixed in the future, for now taking start-end
-        individual_split = individual.split("_")
-        (start, end) = individual_split[3].split("-")
+        # Haploblock hash
+        parts = individual.split("_")
+        start, end = parts[3].split("-")
         haploblock_hash = haploblock2hash[(start, end)]
 
-        # cluster hash
-        cluster = individual2cluster[individual]
+        # Cluster and variant hashes
         cluster_hash = cluster2hash[cluster]
-
-        # variant hash
         variant_hash = variant2hash[individual]
 
-        # individual hash
-        hash = strand_hash + chr_hash + haploblock_hash + cluster_hash + variant_hash
-        individual2hash[individual] = hash
+        # Full individual hash
+        full_hash = strand_hash + chr_hash + haploblock_hash + cluster_hash + variant_hash
+        individual2hash[individual] = full_hash
 
-    return(individual2hash)
+    return individual2hash
 
 
 def hashes_to_TSV(individual2hash, out):
-    '''
-    Save individual to a TSV file, 2 columns: INDIVIDUAL HASH
-
-    arguments:
-    - individual2hash: dict, key=individual, value=hash
-    '''
-    with open(os.path.join(out, f"individual_hashes.tsv"), 'w') as f:
-        # header
+    """
+    Save individual → hash mapping to a TSV file.
+    """
+    output_path = os.path.join(out, "individual_hashes.tsv")
+    with open(output_path, 'w') as f:
         f.write("INDIVIDUAL\tHASH\n")
-        for individual in individual2hash:
-            f.write(individual + "\t" + individual2hash[individual] + "\n")
+        for individual, hash_val in individual2hash.items():
+            f.write(f"{individual}\t{hash_val}\n")
+    logger.info(f"Individual hashes written to {output_path}")
 
 
 def main(clusters_file, variant_hashes, haploblock_hashes, chr, out):
-
     logger.info("Parsing clusters")
-    # this should be fixed, beacuse we will have one clusters file per haploblock, not one clusters file in total
-    (individual2cluster, clusters) = data_parser.parse_clusters(clusters_file)
-    logger.info("Found %i clusters with %i individuals in total", len(clusters), len(individual2cluster))
+    individual2cluster, clusters = data_parser.parse_clusters(clusters_file)
+    logger.info(f"Found {len(clusters)} clusters with {len(individual2cluster)} individuals")
 
     logger.info("Parsing variant hashes")
     variant2hash = data_parser.parse_variant_hashes(variant_hashes)
-    
+
     logger.info("Parsing haploblock hashes")
     haploblock2hash = data_parser.parse_haploblock_hashes(haploblock_hashes)
 
     logger.info("Generating individual hashes")
     chr_hash = numpy.binary_repr(int(chr))
     cluster2hash = generate_cluster_hashes(clusters)
-    individual2hash = generate_individual_hashes(individual2cluster, cluster2hash, variant2hash, haploblock2hash, chr_hash)
+    individual2hash = generate_individual_hashes(
+        individual2cluster, cluster2hash, variant2hash, haploblock2hash, chr_hash
+    )
 
     logger.info("Saving individual hashes")
     hashes_to_TSV(individual2hash, out)
 
+    # Perspective:
+    # ─────────────
+    # # Save also as JSON for programmatic access
+    # import json
+    # with open(os.path.join(out, "individual_hashes.json"), "w") as jf:
+    #     json.dump(individual2hash, jf, indent=2)
+
 
 if __name__ == "__main__":
     script_name = os.path.basename(sys.argv[0])
-    # configure logging, sub-modules will inherit this config
-    logging.basicConfig(format='%(asctime)s %(levelname)s %(name)s: %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S',
-                        level=logging.DEBUG)
-    # set up logger: we want script name rather than 'root'
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)s %(name)s: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        level=logging.DEBUG,
+    )
     logger = logging.getLogger(script_name)
 
     parser = argparse.ArgumentParser(
-        prog=script_name,
-        description="TODO"
+        prog=script_name, description="Generate individual variant hashes per haploblock"
     )
-    
-    parser.add_argument('--clusters',
-                        help='Path to clusters file generated with MMSeqs2',
-                        type=pathlib.Path,
-                        required=True)
-    parser.add_argument('--variant_hashes',
-                        help='Path to file with variant hashes',
-                        type=pathlib.Path,
-                        required=True)
-    parser.add_argument('--haploblock_hashes',
-                        help='Path to file with haploblock hashes',
-                        type=pathlib.Path,
-                        required=True)
-    parser.add_argument('--chr',
-                        help='chromosome',
-                        type=str,
-                        required=True)
-    parser.add_argument('--out',
-                        help='Path to output folder',
-                        type=pathlib.Path,
-                        required=True)
+    parser.add_argument('--clusters', required=True, type=pathlib.Path,
+                        help='Path to clusters file generated with MMSeqs2')
+    parser.add_argument('--variant_hashes', required=True, type=pathlib.Path,
+                        help='Path to file with variant hashes')
+    parser.add_argument('--haploblock_hashes', required=True, type=pathlib.Path,
+                        help='Path to file with haploblock hashes')
+    parser.add_argument('--chr', required=True, type=str, help='Chromosome number')
+    parser.add_argument('--out', required=True, type=pathlib.Path,
+                        help='Output folder path')
 
     args = parser.parse_args()
 
     try:
-        main(clusters_file=args.clusters,
-             variant_hashes=args.variant_hashes,
-             haploblock_hashes=args.haploblock_hashes,
-             chr=args.chr,
-             out=args.out)
-
+        main(
+            clusters_file=args.clusters,
+            variant_hashes=args.variant_hashes,
+            haploblock_hashes=args.haploblock_hashes,
+            chr=args.chr,
+            out=args.out,
+        )
     except Exception as e:
-        # details on the issue should be in the exception name, print it to stderr and die
-        sys.stderr.write("ERROR in " + script_name + " : " + repr(e) + "\n")
+        sys.stderr.write(f"ERROR in {script_name}: {repr(e)}\n")
         sys.exit(1)
+
